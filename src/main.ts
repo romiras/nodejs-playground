19f84fb7f07a002b274ca5302e0d73ed0710db69
Romiras <298544+romiras@users.noreply.github.com>
2021-11-13 22:29:29 +0200

    Basic AMQP consumer

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,52 @@
-import { logger } from './logger'
+import * as lib from 'amqplib';
 
-logger.log('info', 'start')
+const amqpUrl = process.env.AMQP_URL || 'amqp://127.0.0.1:5672';
+const queue = 'foo';
+
+const sleep = time => {
+	return new Promise(resolve => {
+		setTimeout(resolve, time);
+	});
+};
+
+async function callback(body: string) {
+	console.log('Processing body:', body);
+	await sleep(1000);
+	console.log('End processing body:', body);
+
+	return Promise.resolve();
+}
+
+(async () => {
+	let conn: lib.Connection;
+	let channel: lib.Channel;
+
+	try {
+		conn = await lib.connect(amqpUrl);
+		channel = await conn.createChannel();
+	} catch (err) {
+		console.error(err);
+	}
+
+	console.log(` [*] Waiting for messages on queue '${queue}'. To exit press CTRL+C.`);
+	await channel.consume(
+		queue,
+		async (msg: lib.ConsumeMessage) => {
+			if (msg === null) {
+				return;
+			}
+
+			const body = msg.content.toString();
+			console.log(` [x] Received ${body}`);
+
+			try {
+				await callback(body);
+			} catch (error) {
+				console.error(error);
+				return channel.nack(msg);
+			}
+			return channel.ack(msg);
+		},
+		{ noAck: false },
+	);
+})();
