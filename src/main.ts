f56cf49e93f9941eba26b0b00c836affcac38544
Romiras <298544+romiras@users.noreply.github.com>
2021-12-01 00:27:43 +0200

    minimal AWS ES checker

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,32 @@
 import { logger } from './logger'
 
 logger.log('info', 'start')
+
+const { Client } = require('@elastic/elasticsearch');
+const AWS = require('aws-sdk');
+const createAwsElasticsearchConnector = require('aws-elasticsearch-connector');
+
+// (Optional) load profile credentials from file
+// AWS.config.update({
+// 	profile: 'affo-my',
+// 	logger: console,
+// });
+
+// AWS.config.logger = console;
+
+// console.log(JSON.stringify(AWS.config, null, '  '));
+
+const client = new Client({
+	...createAwsElasticsearchConnector(AWS.config),
+	node: process.env.ES_URL,
+});
+
+(async () => {
+	console.log(`Connecting to ${process.env.ES_URL}`);
+	client
+		.ping({}, { requestTimeout: 5000 })
+		.then(res => console.log(res.statusCode))
+		.catch(error => {
+			console.error(error);
+		});
+})();
